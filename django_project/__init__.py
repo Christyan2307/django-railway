96a01423fc4cd1971ef16d1fdb545846d9ada0d5
Christyan2307 <christyanlira3@gmail.com>
2025-10-06 20:56:46 -0300

    Subindo chatbot WhatsApp

--- django_project/__init__.py
+++ django_project/__init__.py
@@ -1,17 +1,3 @@
 import pymysql
 pymysql.install_as_MySQLdb()
 
-import subprocess
-import threading
-import os
-from pathlib import Path
-
-def start_chatbot():
-    try:
-        base_dir = Path(__file__).resolve().parent.parent  # raiz do projeto
-        print("🤖 Iniciando chatbot.js com Node.js...")
-        subprocess.Popen(["node", "chatbot.js"], cwd=base_dir)
-    except Exception as e:
-        print(f"❌ Erro ao iniciar chatbot.js: {e}")
-
-threading.Thread(target=start_chatbot, daemon=True).start()
